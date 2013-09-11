5ff434b2c96ad5355f969abafe03f129170fc50b
Will Strinz <wstrinz@gmail.com>
2013-09-10 22:35:17 -0500

    don't show properties with no entries

--- app/models/property.rb
+++ app/models/property.rb
@@ -15,10 +15,11 @@ class Property < ActiveRecord::Base
 
   def turtle_prefixes
     {
-       :foaf => "http://xmlns.com/foaf/0.1/",
+       rdf: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
        :rdfs => "http://www.w3.org/2000/01/rdf-schema#",
        :owl => "http://www.w3.org/2002/07/owl#",
-       :ontome_prop => "#{base_url}/properties/",
+       :foaf => "http://xmlns.com/foaf/0.1/",
+       :prop => "#{base_url}/properties/",
      }
   end
 
